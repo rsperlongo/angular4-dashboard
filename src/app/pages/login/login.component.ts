ea34f9ce12ea72572c1b06743b1a5829419bc601
Ricardo Tadeu Sperlongo <ricardo_tadeu30@hotmail.com>
2017-09-20 22:00:55 -0300

    remove comments

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -60,39 +60,6 @@ export class LoginComponent implements OnInit {
                data => {this.router.navigate([this.returnUrl])
               });
 
-
-
-
-
-    //var password = $('#password').val();
-    /*
-       var body = {
-            grant_type: 'password',
-            client_id: 'totem.dashboard.web',
-            client_id: 'totem.dashboard.web',,
-            username: username,
-            password: password,
-            scope: 'openid profile offline_access Dashboard.Search',
-          };
-    
-          $.ajax({
-            url: 'http://desenv.autenticador.sp.gov.br/connect/token',
-            type: 'POST',
-            dataType: 'json',
-            contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
-            data: body,
-            success: function(result) {
-              var access_token =  JSON.stringify(result.access_token);
-              localStorage.setItem('Current user', access_token);
-    
-              $(location).attr('href','http://localhost:4200/#/pages/dashboard');
-              //this.router.navigate(['./dashboard']);
-            },
-            error: function(result) {
-              console.log(JSON.stringify(result.responseJSON.error_description));
-            },
-          });
-          */
   }
   retornoLogin(retorno: string) {
     let ret: RetornoLogin = JSON.parse(JSON.stringify(retorno));
@@ -105,30 +72,3 @@ export class LoginComponent implements OnInit {
   }
 }
 
-  /*  public form: FormGroup;
-    public email: AbstractControl;
-    public password: AbstractControl;
-    public submitted: boolean = false;
-  
-    constructor(fb: FormBuilder) {
-      this.form = fb.group({
-        'email': ['', Validators.compose([Validators.required, Validators.minLength(4)])],
-        'password': ['', Validators.compose([Validators.required, Validators.minLength(4)])],
-      });
-  
-      this.email = this.form.controls['email'];
-      this.password = this.form.controls['password'];
-    } 
-    ngOnInit() {
-      let dataatual = moment();
-      console.log(dataatual.format('x'));
-      
-    }
-    /*
-    public onSubmit(values: Object): void {
-      this.submitted = true;
-      if (this.form.valid) {
-        // your code goes here
-        // console.log(values);
-      }
-    } */
